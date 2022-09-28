64a3621d1d803160f70ca7136614a20525559eb1
VSHEVTSOV <shavec3@gmail.com>
2022-09-28 11:37:35 +0300

    Update DeepComparison.js

--- Data Structures (Objects and Arrays)/DeepComparison.js
+++ Data Structures (Objects and Arrays)/DeepComparison.js
@@ -1,12 +1,18 @@
 function deepEqual(firstValue, secondValue) {
     if ( (typeof firstValue == 'object' && firstValue != null) && 
          (typeof secondValue == 'object' && secondValue != null) ) {
-    
-        if (Object.keys(firstValue).length != Object.keys(secondValue).length) {
+
+        if (Object.getOwnPropertyNames(firstValue).length != Object.getOwnPropertyNames(secondValue).length) {
             return false;
         } else {
                 for (let property in firstValue) {
-                    if ( !(property in secondValue) ||
+                    if ( !property in secondValue ||
+                         !deepEqual( firstValue[property], secondValue[property]) ) {
+                            return false;
+                         }
+                }
+                for (let property in secondValue) {
+                    if ( !(property in firstValue) ||
                          !deepEqual( firstValue[property], secondValue[property]) ) {
                             return false;
                     }
@@ -14,11 +20,7 @@ function deepEqual(firstValue, secondValue) {
                 return true;
         }
     } else {
-        if (firstValue !== secondValue) {
-            return false;
-        } else {
-            return true;
-        }
+        return firstValue === secondValue;
     }
 }
 
@@ -28,3 +30,13 @@ console.log( deepEqual(5, 5) );
 console.log( deepEqual(obj, obj) );
 console.log( deepEqual(obj, {here: 1, object: 2}) );
 console.log( deepEqual(obj, {here: {is: 'an'}, object: 2}) );
+
+const a = Object.create(null, {
+    a: { value: 1 },
+    b: { value: 2 },
+    
+});
+
+const b = {a: 1, b: 2};
+
+console.log(deepEqual(a, b));
